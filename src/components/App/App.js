97b9481521061b9c6998d7623b5b11e74f13e907
Avery Yang <averyyang2674@gmail.com>
2022-07-23 18:08:17 -0500

    add details component, refactor code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,21 @@
-import {HashRouter as Router, Route} from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import MovieList from '../MovieList/MovieList'
+import MovieList from '../MovieList/MovieList';
+import Details from '../Details/Details';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>The Movies Saga!</h1>
-      <Router>        
-        <Route path="/" exact>
-          <MovieList />
-        </Route>
-        
-        {/* Details page */}
-
-        {/* Add Movie page */}
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>The Movies Saga!</h1>
+            <Router>
+                <Route exact path="/" component={MovieList} />
+                {/* Details page */}
+                <Route exact path="/details" component={Details} />
+                {/* Add Movie page */}
+                {/* <Route exact path="/addmovie" component={AddMovie} /> */}
+            </Router>
+        </div>
+    );
 }
-
 
 export default App;
